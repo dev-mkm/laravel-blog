49e30fdd4e21493bfd92b59f6ffe9f13d273def2
MKM <32406304+dev-mkm@users.noreply.github.com>
2024-07-23 12:57:32 +0330

    Add Pulse

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,9 @@ class AppServiceProvider extends ServiceProvider
     {
         Blade::if('admin', function () {
             return Auth::user()->admin;
+        });
+        Gate::define('viewPulse', function (User $user) {
+            return $user->admin;
         });
     }
 }
